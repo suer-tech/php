e51941c9b0dafd1823f6054925ffe3f9a27ccb8b
Suer-Tech <102809506+suer-tech@users.noreply.github.com>
2022-11-30 16:11:38 +0500

    functionObject

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -3,5 +3,9 @@ window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surnameOutput').innerText = initPerson.surName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
+
 };
 
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,6 +35,22 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Дарья",
+            "id_3": "Мария",
+            "id_4": "Анна",
+            "id_5": "Дина",
+            "id_6": "Наталья",
+            "id_7": "Милана",
+            "id_8": "Василиса",
+            "id_9": "Ева",
+            "id_10": "Агния"
+        }
+    }`,
+
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -47,24 +63,43 @@ const personGenerator = {
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
+    randomGender: function () {
+        const male = this.randomIntNumber();
+        const gender = male == 1? this.GENDER_MALE : this.GENDER_FEMALE;
+        return gender;                    
+    },
 
-        return this.randomValue(this.firstNameMaleJson);
+    randomFirstName: function () {
+        if (this.person.gender == this.GENDER_MALE) {
+            return this.randomValue(this.firstNameMaleJson);
+        }
+        return this.randomValue(this.firstNameFemaleJson);
+    },
+
+    randomSurname: function () {
+        if (this.person.gender == this.GENDER_MALE) {
+            return this.randomValue(this.surnameJson);
+        }
+        return this.randomValue(this.surnameJson) + 'а';
 
     },
 
-
-     randomSurname: function() {
-
-        return this.randomValue(this.surnameJson);
-
+    birthYear: function () {
+        const year = this.randomIntNumber(2004,1970);
+        const month = this.randomIntNumber(12,1);
+        const day = this.randomIntNumber(28,1);
+        const birth = String(year) + '.' + String(month) + '.' + String(day);
+        return birth;                    
     },
 
-
+  
+    
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surName = this.randomSurname();
+        this.person.birthYear = this.birthYear();
         return this.person;
     }
 };
